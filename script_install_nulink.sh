00b16f798c0fc95ffd252d9a034a1b5e0ef3f310
solotop999 <nhd096@gmail.com>
2024-01-02 21:46:42 +0700

    Update script_install_nulink.sh

--- script_install_nulink.sh
+++ script_install_nulink.sh
@@ -6,8 +6,50 @@ read -p 'Input your password (More than 8 digits): '  PASSWORD
 export NULINK_KEYSTORE_PASSWORD=$PASSWORD
 export NULINK_OPERATOR_ETH_PASSWORD=$PASSWORD
 
-sudo ufw allow 9151
+# Check if the system is Ubuntu
+if ! grep -q "Ubuntu" /etc/lsb-release; then
+    echo "This script is for Ubuntu only. Exiting."
+    exit 1
+fi
 
+
+# Check if the script is not running as root
+if [ "$EUID" -ne 0 ]; then
+    echo "Enter password to Switching to root"
+    sudo su
+fi
+
+
+#install docker
+# Check if Docker is installed
+if command -v docker &> /dev/null; then
+    echo "Docker is already installed. Skipping installation."
+else
+    sudo apt install curl build-essential git screen jq pkg-config libssl-dev libclang-dev ca-certificates gnupg lsb-release -y
+
+    # Install Docker
+    sudo install -m 0755 -d /etc/apt/keyrings
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+    sudo chmod a+r /etc/apt/keyrings/docker.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+      $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+      sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt-get update -y
+    sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin -y
+    systemctl start docker
+    systemctl enable docker
+fi
+
+# Check if Docker Compose is installed
+if command -v docker-compose &> /dev/null; then
+    echo "Docker Compose is already installed. Skipping installation."
+else
+    # Install Docker Compose
+    sudo apt-get install docker-compose -y
+fi
 
 # Create Worker Account
 cd /root
